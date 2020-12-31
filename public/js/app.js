13be806e88dd189d438dde23a4dce2d9e20e093e
Kevin Kayisire <kaykevin1@gmail.com>
2020-12-31 09:57:10 +0200

    updated README file

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\divine\documents\e\cpiv\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\divine\documents\e\cpiv\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/kevin/Desktop/D6/Playground/cpiv/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/kevin/Desktop/D6/Playground/cpiv/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
