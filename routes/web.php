65b7312df9cd3f5b481bb616b588b98617f01588
Kevin Kayisire <kaykevin1@gmail.com>
2021-01-10 12:57:46 +0200

    Few changes

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,18 @@ Route::get('/', [AdminController::class, 'welcome']);
 Route::get('/home', [AdminController::class, 'index'])->middleware('auth');
 
 Route::get('/accounts', [UserController::class, 'index'])->middleware('auth');
-Route::get('/accounts/types/new', [UserController::class, 'newType'])->middleware('auth');
-Route::post('/accounts/types/new', [UserController::class, 'addType'])->middleware('auth')->name('types');
-Route::get('/accounts/types/{id}/activate', [UserController::class, 'activateType'])->middleware('auth');
-Route::get('/accounts/types/{id}/disable', [UserController::class, 'disableType'])->middleware('auth');
-Route::get('/accounts/types', [UserController::class, 'types'])->middleware('auth');
 Route::get('/accounts/{id}/activate', [UserController::class, 'activateAccount'])->middleware('auth');
 Route::get('/accounts/{id}/disable', [UserController::class, 'disableAccount'])->middleware('auth');
-Route::get('/accounts/{id}/assign', [UserController::class, 'assignAccount'])->middleware('auth');
-Route::post('/accounts/assign', [UserController::class, 'makeAssignAccount'])->middleware('auth')->name('assignType');
-Route::get('/accounts/{userId}/assign/{typeId}/remove', [UserController::class, 'removeAssignAccount'])->middleware('auth');
+
+Route::get('/accounts/{id}/assign', [UserByTypeController::class, 'assignAccount'])->middleware('auth');
+Route::post('/accounts/assign', [UserByTypeController::class, 'makeAssignAccount'])->middleware('auth')->name('assignType');
+Route::get('/accounts/{userId}/assign/{typeId}/remove', [UserByTypeController::class, 'removeAssignAccount'])->middleware('auth');
+
+Route::get('/accounts/types/new', [UserTypeController::class, 'newType'])->middleware('auth');
+Route::post('/accounts/types/new', [UserTypeController::class, 'addType'])->middleware('auth')->name('types');
+Route::get('/accounts/types/{id}/activate', [UserTypeController::class, 'activateType'])->middleware('auth');
+Route::get('/accounts/types/{id}/disable', [UserTypeController::class, 'disableType'])->middleware('auth');
+Route::get('/accounts/types', [UserTypeController::class, 'types'])->middleware('auth');
 
 Route::get('/projects', [ProjectController::class, 'index'])->middleware('auth');
 Route::get('/projects/new', [ProjectController::class, 'new'])->middleware('auth');
@@ -31,9 +33,11 @@ Route::post('/projects/new', [ProjectController::class, 'store'])->middleware('a
 Route::get('/projects/pending', [ProjectController::class, 'pending'])->middleware('auth');
 Route::get('/projects/all', [ProjectController::class, 'all'])->middleware('auth');
 Route::get('/projects/{id}/approve', [ProjectController::class, 'approve'])->middleware('auth');
+Route::post('/projects/approve', [ProjectController::class, 'makeApprovement'])->middleware('auth')->name('approveProject');
 Route::get('/projects/{id}/suspend', [ProjectController::class, 'suspend'])->middleware('auth');
 Route::get('/projects/{id}/delete', [ProjectController::class, 'delete'])->middleware('auth');
 Route::get('/projects/{id}/view', [ProjectController::class, 'view'])->middleware('auth');
+Route::get('/projects/{id}/approve/{location}/location', [ProjectController::class, 'approveLocation'])->middleware('auth');
 
 Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth');
 Route::post('/profile', [ProfileController::class, 'store'])->middleware('auth')->name('profile');
